02555642bd3ba3d91750eb120674602465d3d48b
Liam Kinne <liam@kinne.io>
2024-10-26 08:20:25 +1000

    add usb identifiers

---
+++ src/identifier.rs
@@ -0,0 +1,7 @@
+use usb_device::device::UsbVidPid;
+
+pub const GS_USB_1: UsbVidPid = UsbVidPid(0x1d50, 0x606f);
+pub const CANDLELIGHT: UsbVidPid = UsbVidPid(0x1209, 0x2323);
+pub const CES_CANEXT_FD: UsbVidPid = UsbVidPid(0x1cd2, 0x606f);
+pub const ABE_CANDEBUGGER_FD: UsbVidPid = UsbVidPid(0x16d0, 0x10b8);
+pub const XYLANTA_SAINT3: UsbVidPid = UsbVidPid(0x16d0, 0x0f30);
