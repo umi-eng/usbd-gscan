2fd8ca1e417d22a1aff347b66dd1f09c1d180ef6
Liam Kinne <liam@kinne.io>
2024-10-26 08:26:05 +1000

    add existing work

---
+++ src/host.rs
@@ -0,0 +1,264 @@
+//! Host interface messages.
+
+use bitflags::bitflags;
+use zerocopy::{AsBytes, FromBytes, FromZeroes};
+
+/// Tells the device the byte order of the host.
+///
+/// `byte_order` will contain `0x0000beef` for little endian and `0xefbe0000`
+/// for big endian.
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct HostConfig {
+    pub byte_order: u32,
+}
+
+/// Device configuration.
+///
+/// `interface_count`
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceConfig {
+    pub _reserved0: u8,
+    pub _reserved1: u8,
+    pub _reserved2: u8,
+    pub interface_count: u8,
+    pub software_version: u32,
+    pub hardware_version: u32,
+}
+
+impl DeviceConfig {
+    /// Creates a new device config.
+    ///
+    /// The number of interfaces must be at least 1.
+    pub fn new(interfaces: u8) -> Self {
+        assert!(interfaces > 0);
+
+        // API useses N-1 to represent the interface count.
+        let interface_count = interfaces - 1;
+
+        Self {
+            _reserved0: 0,
+            _reserved1: 0,
+            _reserved2: 0,
+            interface_count,
+            software_version: 2, // to match candleLight firmware.
+            hardware_version: 0,
+        }
+    }
+}
+
+/// Device mode.
+#[derive(Debug)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+pub enum Mode {
+    Reset = 0,
+    Start = 1,
+}
+
+impl TryFrom<u32> for Mode {
+    type Error = ();
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        match value {
+            x if x == Self::Reset as u32 => Ok(Self::Reset),
+            x if x == Self::Start as u32 => Ok(Self::Start),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceMode {
+    pub mode: u32,
+    pub flags: Flag, // todo: bitflags
+}
+
+/// Same as Linux netlink can_state.
+#[derive(Debug, Clone, Copy, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(u32)]
+pub enum CanState {
+    /// RX/TX error count < 96
+    Active = 0,
+    /// RX/TX error count < 128
+    Warning = 1,
+    /// RX/TX error count < 256
+    Passive = 2,
+    /// RX/TX error count >= 256
+    BusOff = 3,
+    /// Device is stopped
+    Stopped = 4,
+    /// Device is sleeping
+    Sleeping = 5,
+}
+
+impl Into<u32> for CanState {
+    fn into(self) -> u32 {
+        self as u32
+    }
+}
+
+#[derive(Debug, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceState {
+    pub state: CanState,
+    pub rx_errors: u32,
+    pub tx_errors: u32,
+}
+
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceBitTiming {
+    pub prop_seg: u32,
+    pub phase_seg1: u32,
+    pub phase_seg2: u32,
+    pub sjw: u32,
+    pub brp: u32,
+}
+
+#[derive(Debug, Default, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct CanBitTimingConst {
+    pub tseg1_min: u32,
+    pub tseg1_max: u32,
+    pub tseg2_min: u32,
+    pub tset2_max: u32,
+    pub sjw_max: u32,
+    pub brp_min: u32,
+    pub brp_max: u32,
+    pub brp_inc: u32,
+}
+
+/// Features flags that can be advertised by the device.
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct Flag(u32);
+
+bitflags! {
+    impl Flag: u32 {
+        const LISTEN_ONLY = 1 << 0;
+        const LOOP_BACK = 1 << 1;
+        const TRIPLE_SAMPLE = 1 << 2;
+        const ONE_SHOT = 1 << 3;
+        const HW_TIMESTAMP = 1 << 4;
+        const IDENTIFY = 1 << 5;
+        const USER_ID = 1 << 6;
+        const PAD_PKTS_TO_MAX_PKT_SIZE = 1 << 7;
+        const FD = 1 << 8;
+        const REQ_USB_QUIRK_LPC546XX = 1 << 9;
+        const BT_CONST_EXT = 1 << 10;
+        const TERMINATION = 1 << 11;
+        const BUS_ERROR_REPORTING = 1 << 12;
+        const GET_STATE = 1 << 13;
+    }
+}
+
+/// Device bit timing and feature flags.
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceBitTimingConst {
+    pub features: Flag,
+    pub fclk_can: u32,
+    pub bit_timing: CanBitTimingConst,
+}
+
+/// Device extended bit timing and feature flags for CAN FD devices.
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceBitTimingConstExtended {
+    pub features: Flag,
+    pub fclk_can: u32,
+    pub bit_timing: CanBitTimingConst,
+    pub data_timing: CanBitTimingConst,
+}
+
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct IdentifyMode {
+    pub mode: u32,
+}
+
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct DeviceTerminationState {
+    pub state: u32,
+}
+
+#[derive(Debug, Clone, Copy, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct ClassicCan {
+    pub data: [u8; 8],
+}
+
+#[derive(Debug, Clone, Copy, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct ClassicCanTimestamp {
+    pub data: [u8; 8],
+    pub timestamp_us: u32,
+}
+
+#[derive(Debug, Clone, Copy, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct CanFd {
+    pub data: [u8; 64],
+}
+
+#[derive(Debug, Clone, Copy, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct CanFdTimestamp {
+    pub data: [u8; 64],
+    pub timestamp_us: u32,
+}
+
+#[derive(Clone, Copy, FromZeroes, FromBytes)]
+// #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub union CanData {
+    pub classic_can: ClassicCan,
+    pub classic_can_timestamp: ClassicCanTimestamp,
+    pub can_fd: CanFd,
+    pub can_fd_timestamp: CanFdTimestamp,
+}
+
+#[derive(Clone, Copy, FromZeroes, FromBytes)]
+// #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct Frame {
+    pub echo_id: u32,
+    pub can_id: u32,
+    pub can_dlc: u8,
+    pub channel: u8,
+    pub flags: u8,
+    pub _reserved0: u8,
+    pub can_data: CanData,
+}
+
+#[derive(Debug, Clone, Copy, FromZeroes, FromBytes, AsBytes)]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[repr(C)]
+pub struct CanIdFlags(u32);
+
+bitflags! {
+    impl CanIdFlags: u32 {
+        const EXT_FRAME = 1 << 31;
+        const REMOTE = 1 << 30;
+        const ERROR = 1 << 29;
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,184 @@
+#![no_std]
+
+pub mod host;
+pub mod identifier;
+
+use host::DeviceBitTiming;
+use host::DeviceBitTimingConst;
+use host::DeviceConfig;
+use host::DeviceMode;
+use host::DeviceState;
+use host::Flag;
+use host::HostConfig;
+use usb_device::class_prelude::*;
+use zerocopy::AsBytes;
+use zerocopy::FromBytes;
+
+/// Interface class: vendor defined.
+pub const INTERFACE_CLASS: u8 = 0xFF;
+
+const REQ_HOST_FORMAT: u8 = 0;
+const REQ_BIT_TIMING: u8 = 1;
+const REQ_MODE: u8 = 2;
+const REQ_BUS_ERROR: u8 = 3;
+const REQ_BIT_TIMING_CONST: u8 = 4;
+const REQ_DEVICE_CONFIG: u8 = 5;
+const REQ_TIMESTAMP: u8 = 6;
+const REQ_IDENTIFY: u8 = 7;
+const REQ_GET_USER_ID: u8 = 8;
+const REQ_SET_USER_ID: u8 = 9;
+const REQ_BIT_TIMING_DATA: u8 = 10;
+const REQ_SET_TERMINATION: u8 = 11;
+const REQ_GET_TERMINATION: u8 = 12;
+const REQ_GET_STATE: u8 = 13;
+
+/// Geschwister Schneider USB device.
+pub struct GsCan<'a, B: UsbBus, D: Device> {
+    interface: InterfaceNumber,
+    write_endpoint: EndpointIn<'a, B>,
+    read_endpoint: EndpointOut<'a, B>,
+    device: D,
+}
+
+impl<'a, B: UsbBus, D: Device> GsCan<'a, B, D> {
+    /// Crate a new GsUsb device.
+    pub fn new(alloc: &'a UsbBusAllocator<B>, device: D) -> Self {
+        Self {
+            interface: alloc.interface(),
+            write_endpoint: alloc.bulk(64),
+            read_endpoint: alloc.bulk(64),
+            device,
+        }
+    }
+
+    pub fn read(&mut self) {
+        let mut data = [0; 64];
+        let res = self.read_endpoint.read(&mut data);
+        match res {
+            Ok(_) => defmt::info!("Read from endpoint"),
+            Err(UsbError::WouldBlock) => {}
+            Err(e) => defmt::error!("{}", e),
+        }
+    }
+}
+
+impl<B: UsbBus, D: Device> UsbClass<B> for GsCan<'_, B, D> {
+    fn get_configuration_descriptors(
+        &self,
+        writer: &mut DescriptorWriter,
+    ) -> usb_device::Result<()> {
+        writer.interface(self.interface, INTERFACE_CLASS, 0xFF, 0xFF)?;
+        writer.endpoint(&self.write_endpoint)?;
+        writer.endpoint(&self.read_endpoint)?;
+
+        Ok(())
+    }
+
+    // Handle control requests to the host.
+    fn control_in(&mut self, xfer: ControlIn<B>) {
+        let req = *xfer.request();
+
+        defmt::info!("Control in: {:?}", req);
+
+        if req.request_type != control::RequestType::Vendor {
+            return;
+        }
+
+        match req.request {
+            REQ_DEVICE_CONFIG => {
+                xfer.accept_with(self.device.device_config().as_bytes())
+                    .unwrap();
+            }
+            REQ_BIT_TIMING_CONST => {
+                let config = DeviceBitTimingConst {
+                    features: Flag::FD,
+                    fclk_can: 8_000_000,
+                    bit_timing: host::CanBitTimingConst {
+                        tseg1_min: 0,
+                        tseg1_max: 31,
+                        tseg2_min: 0,
+                        tset2_max: 15,
+                        sjw_max: 15,
+                        brp_min: 0,
+                        brp_max: 15,
+                        brp_inc: 1,
+                    },
+                };
+                xfer.accept_with(config.as_bytes()).unwrap();
+            }
+            REQ_GET_STATE => {
+                xfer.accept_with(self.device.state().as_bytes()).unwrap();
+            }
+            _ => {
+                #[cfg(feature = "defmt-03")]
+                defmt::warn!("Unimplemented request kind: {}", req.request);
+            }
+        }
+    }
+
+    // Handle control requests from the host
+    fn control_out(&mut self, xfer: ControlOut<B>) {
+        let req = *xfer.request();
+
+        defmt::info!("Control out: {:?}", req);
+
+        if req.request_type != control::RequestType::Vendor {
+            return;
+        }
+
+        match req.request {
+            REQ_HOST_FORMAT => {
+                let config = HostConfig::ref_from(xfer.data()).unwrap();
+                assert_eq!(
+                    config.byte_order, 0x0000beef,
+                    "Byte order check mismatch. Big endian not currently supported.",
+                );
+                xfer.accept().ok();
+            }
+            REQ_BIT_TIMING => {
+                let timing = DeviceBitTiming::read_from(xfer.data()).unwrap();
+                let interface = req.value;
+
+                self.device.device_bit_timing(interface, timing);
+                xfer.accept().unwrap();
+            }
+            REQ_MODE => {
+                let device_mode = DeviceMode::ref_from(xfer.data()).unwrap();
+
+                let interface = req.value;
+                let mode = host::Mode::try_from(device_mode.mode).unwrap();
+
+                match mode {
+                    host::Mode::Reset => self.device.reset(interface),
+                    host::Mode::Start => self.device.start(interface),
+                }
+
+                xfer.accept().ok();
+            }
+            _ => {
+                #[cfg(feature = "defmt-03")]
+                defmt::warn!("Unimplemented request kind: {}", req.request);
+                xfer.reject().ok();
+            }
+        }
+    }
+}
+
+pub trait Device {
+    /// Returns the device configuration.
+    ///
+    /// Requested after reset by the host.
+    fn device_config(&self) -> DeviceConfig;
+
+    /// Called to configure the timing of the CAN interface.
+    fn device_bit_timing(&mut self, interface: u16, timing: DeviceBitTiming);
+
+    /// Called when the host requests an interface is reset.
+    fn reset(&mut self, interface: u16);
+
+    /// Called when the host requests an interface is started.
+    fn start(&mut self, interface: u16);
+
+    /// Returns the device state including TX and RX error counters.
+    fn state(&self) -> DeviceState;
+}
